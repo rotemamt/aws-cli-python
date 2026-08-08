fe5388cb24ec3155400928f9486b168f57d91baa
rotemamt <rotemamt@gmail.com>
2026-08-08 23:22:35 +0300

    Wire EC2 create/list/start/stop into CLI with argument validation

--- aws/ec2.py
+++ aws/ec2.py
@@ -1,14 +1,136 @@
 import boto3
-from config import CREATED_BY_TAG_KEY, CREATED_BY_TAG_VALUE, OWNER
+from config import ALLOWED_INSTANCE_TYPES, MAX_RUNNING_INSTANCES, CREATED_BY_TAG_KEY, CREATED_BY_TAG_VALUE, OWNER_TAG_KEY, OWNER_TAG_VALUE, UBUNTU_AMI_PARAMETER, AMAZON_LINUX_AMI_PARAMETER, PROJECT_TAG_KEY, PROJECT_TAG_VALUE, ENVIRONMENT_TAG_KEY, ENVIRONMENT_TAG_VALUE
+
 def list_instances():
     client = boto3.client("ec2")
     response = client.describe_instances(
         Filters=[
             {"Name": f"tag:{CREATED_BY_TAG_KEY}", "Values": [CREATED_BY_TAG_VALUE]},
-            {"Name": "tag:Owner", "Values": [OWNER]}
+            {"Name": f"tag:{OWNER_TAG_KEY}", "Values": [OWNER_TAG_VALUE]}
         ]
     )
-
+    count = 0
     for reservation in response["Reservations"]:
         for instance in reservation["Instances"]:
             print(instance["InstanceId"], instance["InstanceType"], instance["State"]["Name"])
+            count += 1
+    if count == 0:
+        print("No instances found.")
+
+
+def count_running_instances():
+    client = boto3.client("ec2")
+    response = client.describe_instances(
+        Filters=[
+            {"Name": f"tag:{CREATED_BY_TAG_KEY}", "Values": [CREATED_BY_TAG_VALUE]},
+            {"Name": f"tag:{OWNER_TAG_KEY}", "Values": [OWNER_TAG_VALUE]},
+            {"Name": "instance-state-name", "Values": ["running", "pending"]}
+        ]
+    )
+    count = 0
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+            count += 1
+    return count
+
+
+def get_latest_ami(os_name):
+    if os_name == "ubuntu":
+        parameter_name = UBUNTU_AMI_PARAMETER
+    else:
+        parameter_name = AMAZON_LINUX_AMI_PARAMETER
+
+    client = boto3.client("ssm")
+    response = client.get_parameter(Name=parameter_name)
+    return response["Parameter"]["Value"]
+
+
+def create_instance(instance_type, os_name):
+    if instance_type not in ALLOWED_INSTANCE_TYPES:
+        print(f"Error: {instance_type} not allowed.")
+        return
+    current = count_running_instances()
+    if current >= MAX_RUNNING_INSTANCES:
+        print(f"Error: {MAX_RUNNING_INSTANCES} are already running, you've hit the limit.")
+        return
+    ami_id = get_latest_ami(os_name)
+    client = boto3.client("ec2")
+    response = client.run_instances(
+        ImageId=ami_id,
+        InstanceType=instance_type,
+        MinCount=1,
+        MaxCount=1,
+        TagSpecifications=[
+            {
+                "ResourceType": "instance",
+                "Tags":[
+                    {"Key": CREATED_BY_TAG_KEY, "Value": CREATED_BY_TAG_VALUE},
+                    {"Key": OWNER_TAG_KEY, "Value": OWNER_TAG_VALUE},
+                    {"Key": PROJECT_TAG_KEY, "Value": PROJECT_TAG_VALUE},
+                    {"Key": ENVIRONMENT_TAG_KEY , "Value": ENVIRONMENT_TAG_VALUE}
+
+                ]
+            }
+        ]
+    )
+    instance_id = response["Instances"][0]["InstanceId"]
+    print(f"Instance Id: {instance_id}, Instance type: {instance_type}, AMI: {ami_id}")
+
+
+def stop_instance(instance_id):
+    client = boto3.client("ec2")
+    response = client.describe_instances(InstanceIds=[instance_id])
+    instance = response["Reservations"][0]["Instances"][0]
+    tags = instance["Tags"]
+
+    has_created_by = False
+    has_owner = False
+
+    for tag in tags:
+        if tag["Key"] == CREATED_BY_TAG_KEY and tag["Value"] == CREATED_BY_TAG_VALUE:
+            has_created_by = True
+        if tag["Key"] == OWNER_TAG_KEY and tag["Value"] == OWNER_TAG_VALUE:
+            has_owner = True
+
+    if not has_created_by or not has_owner:
+        print(f"Error: {instance_id} was not created by this CLI. Refusing to stop it.")
+        return
+
+    state = instance["State"]["Name"]
+    if state in ["stopped", "stopping"]:
+        print(f"{instance_id} is already {state}.")
+        return
+    if state in ["terminated", "shutting-down"]:
+        print(f"{instance_id} is already {state}.")
+        return
+
+    client.stop_instances(InstanceIds=[instance_id])
+    print(f"Stopping {instance_id}")
+
+
+def start_instance(instance_id):
+    client = boto3.client("ec2")
+    response = client.describe_instances(InstanceIds=[instance_id])
+    instance = response["Reservations"][0]["Instances"][0]
+    tags = instance["Tags"]
+
+    has_created_by = False
+    has_owner = False
+
+    for tag in tags:
+        if tag["Key"] == CREATED_BY_TAG_KEY and tag["Value"] == CREATED_BY_TAG_VALUE:
+            has_created_by = True
+        if tag["Key"] == OWNER_TAG_KEY and tag["Value"] == OWNER_TAG_VALUE:
+            has_owner = True
+
+    if not has_created_by or not has_owner:
+        print(f"Error: {instance_id} was not created by this CLI. Refusing to start it.")
+        return
+
+    state = instance["State"]["Name"]
+    if state != "stopped":
+        print(f"Cannot start {instance_id}. Instance is already {state}.")
+        return
+
+    client.start_instances(InstanceIds=[instance_id])
+    print(f"Starting {instance_id}")
--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-import argparse
+import argparse, sys
+from config import ALLOWED_INSTANCE_TYPES
+from aws.ec2 import create_instance, list_instances, start_instance, stop_instance
+
+
 parser = argparse.ArgumentParser(description="AWS self-service CLI tool")
 parser.add_argument("resource", choices=["ec2", "s3", "route53"], help="Which AWS service to use")
 parser.add_argument("action", choices=["list", "create", "stop", "start", "update", "upload","delete"], help="Action to perform on the resource")
+parser.add_argument("--type", choices=ALLOWED_INSTANCE_TYPES, help="EC2 instance type")
+parser.add_argument("--os", choices=["ubuntu", "amazon-linux"], default="ubuntu", help="Operating system")
+parser.add_argument("--id", help="Instance ID")
+
 args = parser.parse_args()
-print(f"Resource: {args.resource}, Action: {args.action}")
+
+
+if args.resource == "ec2" and args.action in ["start", "stop"] and args.id is None:
+    print("Error: --id is required for start and stop")
+    sys.exit(1)
+
+if args.resource == "ec2" and args.action == "create" and args.type is None:
+    print("Error: --type is required for create")
+    sys.exit(1)
+
+
+if args.resource == "ec2":
+    if args.action == "list":
+        list_instances()
+    elif args.action == "create":
+        create_instance(args.type, args.os)
+    elif args.action == "start":
+        start_instance(args.id)
+    elif args.action == "stop":
+        stop_instance(args.id)
+    else:
+        print(f"Action '{args.action}' is not supported for ec2.")
